552999dcc8a22d6aa65fe38c6b8037a03e7db8cb
Reza Zulfan <39407416+azerliquid@users.noreply.github.com>
2019-03-08 13:36:36 +0700

    Create Tugas_1.java

---
+++ Tugas_1.java
@@ -0,0 +1,6 @@
+package tugas1;
+public class Tugas1 {
+  public static void main(String[] args) {
+    System.out.println("Nama : Reza Zulfan Azmi\n" + "Nim : 17090134\n" + "Kelas : 4c" );
+    }
+}
